b91539c28e586b8fd5af8660c92bd09cdd0d9633
Naufaliaaa <naufalzul47@gmail.com>
2025-06-11 20:24:34 +0700

    Nambah Dropdown

--- app/src/main/java/id/ac/unpas/blogging/ui/features/create_edit_post/CreateEditPost.kt
+++ app/src/main/java/id/ac/unpas/blogging/ui/features/create_edit_post/CreateEditPost.kt
@@ -1,5 +1,6 @@
 package id.ac.unpas.blogging.ui.features.create_edit_post
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
@@ -7,12 +8,16 @@ import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import id.ac.unpas.blogging.ui.theme.BloggingTheme
 import id.ac.unpas.blogging.viewmodel.CreateEditPostViewModel
+import androidx.compose.material3.TextFieldDefaults
+import id.ac.unpas.blogging.ui.theme.White
+
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -79,6 +84,57 @@ fun CreateEditPostScreen(
                     isError = uiState.error != null && uiState.title.isBlank()
                 )
                 Spacer(modifier = Modifier.height(16.dp))
+
+                // Dropdown State
+                val categories = listOf("Teknologi", "Kesehatan", "Gaya Hidup", "Pendidikan")
+                var expanded by remember { mutableStateOf(false) }
+                var selectedCategory by remember { mutableStateOf(categories.first()) }
+
+                ExposedDropdownMenuBox(
+                    expanded = expanded,
+                    onExpandedChange = { expanded = !expanded }
+                ) {
+                    TextField(
+                        value = selectedCategory,
+                        onValueChange = {},
+                        readOnly = true,
+                        label = { Text("Kategori Postingan", color = Color.Black) },
+                        trailingIcon = {
+                            ExposedDropdownMenuDefaults.TrailingIcon(expanded)
+                        },
+                        colors = TextFieldDefaults.colors(
+                            focusedContainerColor = Color.LightGray,
+                            unfocusedContainerColor =  Color.LightGray ,
+                            focusedTextColor = Color.Black,
+                            unfocusedTextColor = Color.Black,
+                            focusedLabelColor = Color.DarkGray,
+                            unfocusedLabelColor = Color.Gray,
+                            focusedIndicatorColor = Color.LightGray,
+                            unfocusedIndicatorColor = Color.LightGray
+                        ),
+                        modifier = Modifier
+                            .menuAnchor()
+                            .fillMaxWidth()
+                    )
+
+                    ExposedDropdownMenu(
+                        expanded = expanded,
+                        onDismissRequest = { expanded = false }
+                    ) {
+                        categories.forEach { category ->
+                            DropdownMenuItem(
+                                text = { Text(category, color = Color.Black) },
+                                onClick = {
+                                    selectedCategory = category
+                                    expanded = false
+                                }
+                            )
+                        }
+                    }
+                }
+
+                Spacer(modifier = Modifier.height(16.dp))
+
 
                 OutlinedTextField(
                     value = uiState.content,
